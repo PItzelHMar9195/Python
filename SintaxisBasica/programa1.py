71b0bd90cd231fb0b8dafc1df6ded0d9ad3f15f3
PamMar9195 <132734333+PamMar9195@users.noreply.github.com>
2024-11-15 20:57:25 -0600

    Modified programa1

---
+++ SintaxisBasica/programa1.py
@@ -0,0 +1,14 @@
+"""
+* Escribe un programa que solicite tu nombre y tu 
+* ciudad de residencia, luego imprímelo en una sola 
+* frase.
+"""
+
+#Mensaje de bienvenida
+print("\n\n\t\tWelcome to my channel. I'm your teacher.\n")
+#Lectura del nobmre y cuidad
+name = input("\t\tHi. What is your name? ")
+city = input("\t\tAnd where are you from? ")
+
+#Se imprime el mensaje
+print("\t\tNice to meet you",name, "Really, I'm from", city, "too!")
