2bf4143756120b12b95e6126024b9758d0e2ebd9
Pamela Itzel Hernández Martínez <itzelhmartinez1995@gmail.com>
2024-12-01 15:37:05 -0600

    Create programa11

---
+++ SintaxisBasica/programa11.py
@@ -0,0 +1,13 @@
+"""
+* Haz un programa que reciba tu edad en años
+* y la convierta en meses.
+"""
+
+#Mensaje
+print("\n\n\t\t**** CONVIRTIENDO TU EDAD (AÑOS) EN MESES ****\n")
+
+#Lectura de datos
+edad = int(input("\t\tINGRESA TU EDAD: "))
+
+#Se muestra la edad en meses:
+print(f"\n\t\tTIENES {edad * 12} MESES DE EDAD\n")
