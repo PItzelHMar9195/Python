658658462ef959470d90cd881f6a0a50c0133def
Pamela Itzel Hernández Martínez <itzelhmartinez1995@gmail.com>
2024-12-01 15:31:22 -0600

    Create programa7

---
+++ SintaxisBasica/programa7.py
@@ -0,0 +1,15 @@
+"""
+* Crea un programa que solicite tres números y calcule su promedio.
+"""
+
+#Mensaje
+print("\n\n\t\t**** CALCULADORA DE PROMEDIOS ****\n")
+
+#Lectura de datos
+numero1 = int(input("\t\tINGRESA EL VALOR 1: "))
+numero2 = int(input("\t\tINGRESA EL VALOR 2: "))
+numero3 = int(input("\t\tINGRESA EL VALOR 3: "))
+
+#Se calcula el promedio
+#Método 2
+print(f"\n\t\tEL PROMEDIO ES: {(numero1 + numero2 + numero3) / 3}\n")
