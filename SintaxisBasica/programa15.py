c0a71a31ba3fa4aeb84646dd489bb35e3eedfe84
Pamela Itzel Hernández Martínez <itzelhmartinez1995@gmail.com>
2024-12-01 15:41:30 -0600

    Create programa15

---
+++ SintaxisBasica/programa15.py
@@ -0,0 +1,15 @@
+"""
+* Haz un programa que reciba un número
+* y muestre su raíz cuadrada.
+"""
+
+import math
+
+#Mensaje 
+print("\n\n\t\t **** CALCULANDO LA RAÍZ CUADRADA ****\n")
+
+#Lectura de datos
+numero = float(input("\t\tINGRESA UN NÚMERO: "))
+
+#Se realiza la operación
+print(f"\n\t\tLA RAÍZ CUADRADA ES: {math.sqrt(numero)}\n")
