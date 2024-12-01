a2b3cf5168a1e4545fe20990e26748aef67048e4
Pamela Itzel Hernández Martínez <itzelhmartinez1995@gmail.com>
2024-12-01 15:40:29 -0600

    Create programa14

---
+++ SintaxisBasica/programa14.py
@@ -0,0 +1,17 @@
+"""
+* Escribe un programa que reciba dos números y 
+* muestre el resultado de sumarlos y multiplicarlos.
+"""
+
+#Mensaje
+print("\n\n\t\t**** CALCULADORA BÁSICA ****\n")
+
+#Lectura de datos
+valorA = float(input("\t\tINGRESA EL VALOR A: "))
+valorB = float(input("\t\tINGRESA EL VALOR B: "))
+
+#Se muestran las operaciones
+print(f"\n\t\t{valorA} + {valorB} = {valorA + valorB}")
+print(f"\t\t{valorA} - {valorB} = {valorA - valorB}")
+print(f"\t\t{valorA} * {valorB} = {valorA * valorB}")
+print(f"\t\t{valorA} / {valorB} = {valorA / valorB}\n")
