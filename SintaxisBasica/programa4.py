e12fce6b872aa8fe9ce7eedd263df83083ec4b53
Pamela Itzel Hernández Martínez <itzelhmartinez1995@gmail.com>
2024-12-01 13:50:36 -0600

    Create programa4

---
+++ SintaxisBasica/programa4.py
@@ -0,0 +1,13 @@
+"""
+* Escribe un programa que convierta grados 
+* Celsius a Fahrenheit.
+"""
+
+#Mensaje de bienvenida
+print("\n\n\t\tCONVERSIÓN DE GRADOS C° A F\n")
+
+#Lectura de datos
+gradosC = int(input("\t\tINGRESA LA TEMPERATURA: "))
+
+#Se muestra la conversión
+print(f"\n\t\tLA TEMPERATURA EN [F] ES {(gradosC * 1.8) + 32}\n")
