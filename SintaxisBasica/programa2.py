33a80b5c66b808f4347ca31f65172b250b7ebf58
Pamela Itzel Hernández Martínez <itzelhmartinez1995@gmail.com>
2024-12-01 13:24:56 -0600

    Create programa2

---
+++ SintaxisBasica/programa2.py
@@ -0,0 +1,20 @@
+"""
+* Calcula el área de un rectángulo. 
+* Solicita la base y la altura como datos.
+"""
+
+#Mensaje para solicituar datos
+print("\n\n\t\t**** ÁREA DEL RECTÁNGULO ****\n")
+
+#Lectura de datos
+base = int(input("\t\tINGRESA LA BASE: "))
+altura = int(input("\t\tINGRESA LA ALTURA: "))
+
+#Se calcula el resultado y se muestra en pantalla
+
+#Primer método
+#area = base * altura
+#print(f"\n\t\tEL ÁREA ES: {area}\n")
+
+#Segundo método
+print(f"\n\t\tEL ÁREA ES: {base * altura}\n")
