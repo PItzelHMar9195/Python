c036497dc2f5f84e09f7095fd5a729e325368e61
Pamela Itzel Hernández Martínez <itzelhmartinez1995@gmail.com>
2024-12-02 01:59:19 -0600

    Create programa18

---
+++ SintaxisBasica/programa18.py
@@ -0,0 +1,13 @@
+"""
+* Solicita tu altura en metros y 
+* conviértela a centímetros.
+"""
+
+#Mensaje
+print("\n\n\t\t**** CONVERSOR DE METROS A CENTÍMETROS ****\n")
+
+#Lectura de datos
+altura = float(input("\t\tINGRESA TU ALTURA EN METROS: "))
+
+#Conversión
+print(f"\n\t\tTU ALTURA EN CENTÍMETROS SON: {int(altura * 100)}\n")
