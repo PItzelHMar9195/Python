a90c597ae194abdede127fff78d4846a3ebe6848
Pamela Itzel Hernández Martínez <itzelhmartinez1995@gmail.com>
2024-12-02 01:59:55 -0600

    Create programa20

---
+++ SintaxisBasica/programa20.py
@@ -0,0 +1,17 @@
+"""
+* Escribe un programa que reciba dos números y 
+* muestre la división y el residuo de la división.
+"""
+
+#Mensaje
+print("\n\n\t\t**** CALCULADORA DE DIVISIONES ****\n")
+
+#Lectura de datos
+valorA = float(input("\t\tINGRESA EL VALOR DE A: "))
+valorB = float(input("\t\tINGRESA EL VALOR DE B: "))
+
+#Se realizan las operaciones
+print(f"\n\t\t{valorA} / {valorB} = {valorA / valorB}")
+print(f"\t\t{valorA} % {valorB} = {valorA % valorB}\n")
+
+#print(f"\t\t{valorA} % {valorB} = {valorA % valorB}")
