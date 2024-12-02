b840097e7637e112c15b2da38b232aae37c056db
Pamela Itzel Hernández Martínez <itzelhmartinez1995@gmail.com>
2024-12-02 02:11:12 -0600

    Create programa25

---
+++ SintaxisBasica/programa25.py
@@ -0,0 +1,12 @@
+"""
+* Crea un programa que solicite una
+* palabra y muestre su longitud.
+"""
+
+#Mensaje
+print("\n\n\t\t**** CALCULANDO LA LONGITUD DE PALABRAS ****\n")
+
+#Lectura de datos
+palabra = input("\t\tINGRESA UNA PALABRA: ")
+
+print(f"\n\t\tLA PALABRA {palabra} TIENE {len(palabra)} CARACTERES\n")
