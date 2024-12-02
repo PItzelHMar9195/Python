0a1bc43a631f689a530ba05b83541cb7b6561f80
Pamela Itzel Hernández Martínez <itzelhmartinez1995@gmail.com>
2024-12-02 02:13:51 -0600

    Create programa31

---
+++ SintaxisBasica/programa31.py
@@ -0,0 +1,13 @@
+"""
+* Haz un programa que reciba un número decimal y
+* lo convierta a binario.
+"""
+
+#Mensaje
+print("\n\n\t\t**** CONVERSIÓN DE DECIMAL A BINARIO ****\n")
+
+#Lectura de datos
+numero = int(input("\t\tDIGITA UN NÚMERO: "))
+
+
+print(f"\n\t\tEL NÚMERO EN BINARIO ES: {bin(numero)}")
